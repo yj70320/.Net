0acdd20d19cf64eff8b9be6aa7b16a5cda1cb704
Yijun <118766048+yj70320@users.noreply.github.com>
2025-10-08 08:32:03 -0500

    update Infrastructure
    Add DbContext, Services and Repositories

---
+++ MovieShop/Infrastructure/Repositories/MovieRepository.cs
@@ -0,0 +1,30 @@
+﻿using ApplicationCore.Contracts.Repositories;
+using ApplicationCore.Entities;
+using Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repositories
+{
+    public class MovieRepository : Repository<Movie>, IMovieRepository
+    {
+        public MovieRepository(MovieShopDbContext dbContext) : base(dbContext)
+        {
+        }
+
+        public IEnumerable<Movie> Get30HighestGrossingMovie()
+        {
+            // select top 30 * from Movie order by Revenue desc
+            var movies = _dbContext.Movies.OrderByDescending(m => m.Revenue).Take(30).ToList();
+            return movies;
+        }
+
+        public IEnumerable<Movie> Get30HighestRatedMovie()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MovieShop/Infrastructure/Services/MovieService.cs
@@ -0,0 +1,39 @@
+﻿using ApplicationCore.Contracts.Repositories;
+using ApplicationCore.Contracts.Services;
+using ApplicationCore.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Services
+{
+    public class MovieService : IMovieService
+    {
+        // 保存注入的仓储对象
+        private readonly IMovieRepository _movieRepository;
+        public MovieService(IMovieRepository movieRepository)
+        {
+            _movieRepository = movieRepository;
+        }
+        public List<MovieCard> Get30HighestGrossingMovies()
+        {
+            //var movies = new List<MovieCard>
+            //{
+            //    new MovieCard { Title = "Inception", Id = 1, PosterUrl = "https://image.tmdb.org/t/p/w342/9gk7adHYeDvHkCSEqAvQlN1V9Su.jpg" },
+            //    new MovieCard { Title = "Interstellar", Id = 2, PosterUrl = "https://image.tmdb.org/t/p/w342/gEU2QniE6E77NI6lCU6MxlNBvIx.jpg" },
+            //    new MovieCard { Title = "The Dark Knight", Id = 3, PosterUrl = "https://image.tmdb.org/t/p/w342/qJ2tW6WMUDux911r6m7haRef0WH.jpg" },
+            //    new MovieCard { Title = "Deadpool", Id = 4, PosterUrl = "https://image.tmdb.org/t/p/w342/yGSxMiF0cYuAiyuve5DA6bnWbkm.jpg" },
+            //    new MovieCard { Title = "The Avengers", Id = 5, PosterUrl = "https://image.tmdb.org/t/p/w342/RvYMy2wcKCBAz24UyPD7xwmjaTn.jpg" }
+            //};
+            var movies = _movieRepository.Get30HighestGrossingMovie();
+            var movieCards = new List<MovieCard>();
+            foreach (var movie in movies) { 
+                movieCards.Add(new MovieCard { Id = movie.Id , Title = movie.Title, PosterUrl = movie.PosterUrl});
+            };
+            return movieCards;
+        }
+    }
+}
